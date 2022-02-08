3ff3fa99ad3cdbcb05a44b63b20a9ed704cf9add
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-02-07 22:22:35 -0800

    Update run_bot.py

--- run_bot.py
+++ run_bot.py
@@ -29,14 +29,14 @@ def main():
                 if len(order_token) > 2:
                     for power2 in game.powers:
                         #check if the order's unit is in power unit list
-                        if power2 != power_name and not power2.is_eliminated() and order_token[2] in power2.units:
+                        if power2 != power_name and not game.powers[power2].is_eliminated() and order_token[2] in game.powers[power2].units:
                             rec_power = power2
                             
                 # An order is to Support other power
                 if order_token[1] =='S':
                     # send fact - support message
                     press_message = order
-                    power_name.game.new_power_message(rec_power, press_message)
+                    game.powers[power_name].game.new_power_message(rec_power, press_message)
             game.set_orders(power_name, power_orders)
         game.process()
 
