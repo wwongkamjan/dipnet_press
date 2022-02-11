484490a3e4cb74763157682791b2bb831541594a
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-02-11 03:46:03 -0800

    Update bot_communication.py

--- bot_communication.py
+++ bot_communication.py
@@ -1,1 +1,10 @@
 # script as helper for power-power communication
+# class Player:
+# messages tracking sending/receiving/limitation/to_reply/generate all possible messages/replies
+# set_player - as DipNet/DORA/ random any thing
+
+# main():
+# set game, player for each power
+# Player - get_orders/ get_messages/ get_replies
+# Game - set_orders/ add_messages
+# game.process()
