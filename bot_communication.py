d835de31fc050d4f48138442d9a4d7a620cf71f6
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-02-11 21:44:24 -0800

    Update bot_communication.py

--- bot_communication.py
+++ bot_communication.py
@@ -1,6 +1,8 @@
 # script as helper for power-power communication
 # class Player:
 # messages tracking sending/receiving/limitation/to_reply/generate all possible messages/replies
+### generate all possible messages can be classified into groups (attacking) (supporting) (moving/holding/convoying) (proposals - (to attack) (to support) (to move/hold/convoy))
+### generate all possible replies for non-proposal (okay) for proposal (yes,no,maybe)
 # set_player - as DipNet/DORA/ random any thing
 
 # main():
