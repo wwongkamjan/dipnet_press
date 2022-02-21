87eb32f7bacff98a25f671f260758924bcf7e52e
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-02-20 21:36:02 -0800

    Update bot_communication.py

--- bot_communication.py
+++ bot_communication.py
@@ -61,12 +61,11 @@ class Diplomacy_Press:
   def new_message(self, DAIDE_message):
     self.game.add_message(DAIDE_message)
   
-  @gen.coroutine
   def get_all_possible_message(self, sender, recipient):
     # include no message!
     # at first, moves -> then proposal allies, enemies -> then XDO ...
     possible_messages = ['None']
-    orders = yield {sender: self.player.get_orders(self.game, sender)}
+    orders = await {sender: self.player.get_orders(self.game, sender)}
 #     print(orders[sender])
     possible_messages.append(' AND '.join(orders[sender])) #get_non-attacking_orders
     return possible_messages
@@ -83,8 +82,8 @@ class Diplomacy_Press:
   def send_message(self, sender, recipient):
     # number of messages is not exceed limitation (e.g. 6 per phases) and the last message is replied by this recipient or never send to this recipient
     if self.number_sent_msg[sender] <  self.number_msg_limitation and self.sent[sender][recipient]==None:
-      msg_list = yield self.get_all_possible_message(sender, recipient)
-      message = yield self.player.get_message(self.game, msg_list, sender, recipient)
+      msg_list = self.get_all_possible_message(sender, recipient)
+      message = self.player.get_message(self.game, msg_list, sender, recipient)
       if message != "None":
         msg = Message(sender=sender,
              recipient=recipient,
