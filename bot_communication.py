feec350ce80c4d64ec37c707b3c402072dd823ff
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-02-20 20:49:22 -0800

    Update bot_communication.py

--- bot_communication.py
+++ bot_communication.py
@@ -65,9 +65,13 @@ class Diplomacy_Press:
     # at first, moves -> then proposal allies, enemies -> then XDO ...
     possible_messages = ['None']
     orders = self.player.get_orders(self.game, sender)
-    print("orders of ", sender, " : ", orders)
-    orders = ' AND '.join(orders)
-    possible_messages.append(orders) #get_non-attacking_orders
+    str_orders = ''
+    for i in range (len(orders)):
+      str_orders += '( ' + orders[i] + ' )'
+      if i< len(orders)-1:
+        str_orders+= ' AND '
+    
+    possible_messages.append(str_orders) #get_non-attacking_orders
     return possible_messages
   
   def get_all_possible_replies(self, sender, recipient):
@@ -151,16 +155,16 @@ def main():
   dip_game =  Diplomacy_Press(Game=Game(), Player=dip_player)
   while not dip_game.game.is_game_done:
     #send messages before taking orders
-#     for sender in dip_game.powers:
-#       for recipient in dip_game.powers:
-#         if sender != recipient:
-#           print("test")
-#           dip_game.send_message(sender, recipient)
-#     #reply to messages - game/allies/enemy state (or stance) can be changed after getting messages and replies
-#     for sender in dip_game.powers:
-#       for recipient in dip_game.powers:
-#         if sender != recipient:
-#           dip_game.reply_message(sender, recipient)
+    for sender in dip_game.powers:
+      for recipient in dip_game.powers:
+        if sender != recipient:
+          print("test")
+          dip_game.send_message(sender, recipient)
+    #reply to messages - game/allies/enemy state (or stance) can be changed after getting messages and replies
+    for sender in dip_game.powers:
+      for recipient in dip_game.powers:
+        if sender != recipient:
+          dip_game.reply_message(sender, recipient)
                              
     #taking orders after messages were all sent
     orders = yield {power_name: dip_player.get_orders(dip_game.game, power_name) for power_name in dip_game.powers}
