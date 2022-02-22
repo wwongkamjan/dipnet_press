bc74c6167fe7d124213d43cb73c816443189ce5d
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-02-21 22:49:58 -0800

    Create random_bot.py

---
+++ random_bot.py
@@ -0,0 +1,20 @@
+import asyncio
+import random
+from diplomacy.client.connection import connect
+from diplomacy.utils import exceptions
+import argparse
+
+POWERS = ['AUSTRIA', 'ENGLAND', 'FRANCE', 'GERMANY', 'ITALY', 'RUSSIA', 'TURKEY']
+
+class random_player():
+  
+  #get random orders
+  def get_orders(self, game, power_name):
+    if game.get_orderable_locations(power_name):
+        possible_orders = game.get_all_possible_orders()
+        orders = [random.choice(possible_orders[loc]) for loc in game.get_orderable_locations(power_name)
+                  if possible_orders[loc]]
+        return orders
+      
+  def get_message(self, game, msg_list, sender, recipient):
+    return random.choice(msg_list)
