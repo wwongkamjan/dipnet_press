83218de4af1f5030b22656cc59d4a80137e56fff
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-02-21 16:44:55 -0800

    Update bot_communication.py

--- bot_communication.py
+++ bot_communication.py
@@ -71,7 +71,7 @@ class Diplomacy_Press:
 #         orders = self.player.get_orders(self.game, sender)      #get_non-attacking_orders
 #         if isinstance(orders,list):
 #           break
-    orders = yield {sender: self.player.get_orders(self.game, sender)}
+    orders = {sender: self.player.get_orders(self.game, sender)}
     for sender,order in orders:
       possible_messages.append(' AND '.join(order))
       print(order)
