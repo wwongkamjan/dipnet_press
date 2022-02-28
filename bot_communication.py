83af479cb0ea11bcbf8723c4cba64136188a793d
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-02-27 22:45:46 -0800

    Update bot_communication.py

--- bot_communication.py
+++ bot_communication.py
@@ -91,7 +91,7 @@ class Diplomacy_Press:
     # number of messages is not exceed limitation (e.g. 6 per phases) and the last message is replied by this recipient or never send to this recipient
     if self.number_sent_msg[sender] <  self.number_msg_limitation and self.sent[sender][recipient]==None:
       msg_list = self.get_all_possible_message(sender, recipient)
-      message = self.player.get_message(self.game, list(msg_list), sender, recipient)
+      message = self.player.get_message(self.game, msg_list, sender, recipient)
       if message != "None":
         msg = Message(sender=sender,
              recipient=recipient,
