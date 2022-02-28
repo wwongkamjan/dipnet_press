e48c06f504e9c68080c4e8b54c865bc7d37f0c48
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-02-28 00:38:17 -0800

    Update bot_communication.py

--- bot_communication.py
+++ bot_communication.py
@@ -109,8 +109,8 @@ class Diplomacy_Press:
     # number of messages is not exceed limitation (e.g. 6 per phases) and the last message is replied by this recipient or never send to this recipient
     if self.number_sent_msg[sender] <  self.number_msg_limitation and self.sent[sender][recipient]==None:
       msg_list = self.get_all_possible_message(sender, recipient)
-      has_message, message  = self.player.get_message(self.game, msg_list, sender, recipient)
-      if has_message:
+      message  = self.player.get_message(self.game, msg_list, sender, recipient)
+      if message:
         msg = Message(sender=sender,
              recipient=recipient,
              message=message,
@@ -172,25 +172,31 @@ class Diplomacy_Press_Player:
 
     # let agent choose message for each category 
     msg_list = self.player.get_message(game, msg_list, sender, recipient)
-    if msg_list ==None:
-      return (False,None)
     
     # join string for sender move
     # AND (FCT (order1)) ((FCT (order2))) ..
-    sender_move_str = [' ( FCT ( '+order+' ) )' for order in msg_list['sender_move']]
-    sender_move_str = ''.join(sender_move_str)
-    
+    message_str = ''
+    if msg_list['sender_move']:
+      sender_move_str = [' ( FCT ( '+order+' ) )' for order in msg_list['sender_move']]
+      sender_move_str = ''.join(sender_move_str)
+      message_str += sender_move_str
     # join string for proposal
     # AND (PRP (order1)) ((FCT (order2))) ..
-    sender_proposal_str = [' ( PRP ( XDO '+order+' ) )' for order in msg_list['sender_proposal']]
-    sender_proposal_str = ''.join(sender_proposal_str)
+    if msg_list['sender_proposal']:
+      sender_proposal_str = [' ( PRP ( XDO '+order+' ) )' for order in msg_list['sender_proposal']]
+      sender_proposal_str = ''.join(sender_proposal_str)
+      message_str += sender_proposal_str
     
     # message from other power that you want to share (agent already select specific power)
-    other_move_str = [' ( FCT ( '+order+' ) )' for order in msg_list['other_move']]
-    other_move_str = ''.join(other_move_str)
-
-    message = 'AND' + sender_move_str + sender_proposal_str + other_move_str
-    return (True, message)
+    if msg_list['other_move']:
+      other_move_str = [' ( FCT ( '+order+' ) )' for order in msg_list['other_move']]
+      other_move_str = ''.join(other_move_str)
+      message_str += other_move_str
+    
+    if len(message_str)==0:
+      return None
+    else:
+      return 'AND' + message_str
 
   def get_reply(self, game, msg_list, sender, recipient):
     return self.random_message_list(msg_list)
