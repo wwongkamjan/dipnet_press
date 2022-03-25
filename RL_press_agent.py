6bdd2a54e62a60dbf8aa67d2de8c7e942db61023
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-03-24 21:33:53 -0700

    Create RL_press_agent.py

---
+++ RL_press_agent.py
@@ -0,0 +1,12 @@
+# env - s,a,r to interact with agent 
+# agent - network A2C
+# trainer - train agent
+# utils
+# ref: by Chenglong Chen pytorch-DRL  https://github.com/ChenglongChen/pytorch-DRL/tree/44b8f082ffb054c6dabead8971b66a7be4c1f9ff
+
+class Environment:
+  def __init__():
+  def reset(): # return initial state
+  def step(action): # return state, reward, done, info
+  self.action_space 
+  self.observation_space 
