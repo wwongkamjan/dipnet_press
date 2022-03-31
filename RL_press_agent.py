dcb1612ce18eb7f6099437e6a0b9e854c2b878de
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-03-30 23:31:48 -0700

    Update RL_press_agent.py

--- RL_press_agent.py
+++ RL_press_agent.py
@@ -2,9 +2,11 @@
 # agent - network A2C
 # trainer - train agent
 # utils
-# ref: by Chenglong Chen pytorch-DRL  https://github.com/ChenglongChen/pytorch-DRL/tree/44b8f082ffb054c6dabead8971b66a7be4c1f9ff
+# ref: 
+#Chenglong Chen pytorch-DRL  https://github.com/ChenglongChen/pytorch-DRL/tree/44b8f082ffb054c6dabead8971b66a7be4c1f9ff
+#Ray Project rllib https://github.com/ray-project/ray/tree/master/rllib
 
-class Environment:
+class DiplomacyEnv:
   def __init__():
   def reset(): # return initial state
   def step(action): # return state, reward, done, info
