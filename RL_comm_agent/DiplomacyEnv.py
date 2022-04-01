8f00651dfd00c417ddb4f81c98f5d9536a14d9ab
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-03-31 22:53:49 -0700

    Update DiplomacyEnv.py

--- RL_comm_agent/DiplomacyEnv.py
+++ RL_comm_agent/DiplomacyEnv.py
@@ -3,12 +3,14 @@
 # trainer - train agent
 # utils
 # ref: 
-#Chenglong Chen pytorch-DRL  https://github.com/ChenglongChen/pytorch-DRL/tree/44b8f082ffb054c6dabead8971b66a7be4c1f9ff
-#Ray Project rllib https://github.com/ray-project/ray/tree/master/rllib
+
 
 class DiplomacyEnv:
   def __init__():
-  def reset(): # return initial state
+    # initialize
+    # self.action_space 
+    # self.observation_space 
+  def reset(): 
+    # return to initial state - Diplomacy game and DipNet reset
   def step(action): # return state, reward, done, info
-  self.action_space 
-  self.observation_space 
+
