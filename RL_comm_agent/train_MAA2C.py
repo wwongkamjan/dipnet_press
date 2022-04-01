bf16fe6bc7975f732209e4f294e122b7dc0b38a7
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-03-31 23:03:24 -0700

    Update train_MAA2C.py

--- RL_comm_agent/train_MAA2C.py
+++ RL_comm_agent/train_MAA2C.py
@@ -1,15 +1,4 @@
-from tornado import gen
-import ujson as json
-from diplomacy import Game
-from diplomacy.engine.message import Message
-from diplomacy.utils.export import to_saved_game_format
-from diplomacy_research.players.benchmark_player import DipNetSLPlayer
-from diplomacy_research.utils.cluster import start_io_loop, stop_io_loop
-from diplomacy_research.models.state_space import get_order_tokens
-from diplomacy.server.server_game import ServerGame
-from diplomacy.daide.requests import RequestBuilder
-from MAA2C import MAA2C
-from common.utils import ma_agg_double_list
+
 
 import sys
 import numpy as np
