822a1ebd3a5380daa39fb5c1edb682998339d508
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-04-09 15:56:56 -0700

    Update MAA2C.py

--- RL_comm_agent/pytorch_DRL/MAA2C.py
+++ RL_comm_agent/pytorch_DRL/MAA2C.py
@@ -6,8 +6,8 @@ from torch.optim import Adam, RMSprop
 import numpy as np
 
 from pytorch_DRL.common.Agent import Agent
-from common.Model import ActorNetwork, CriticNetwork
-from common.utils import entropy, index_to_one_hot, to_tensor_var
+from pytorch_DRL.common.Model import ActorNetwork, CriticNetwork
+from pytorch_DRL.common.utils import entropy, index_to_one_hot, to_tensor_var
 
 
 class MAA2C(Agent):
