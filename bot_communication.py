f5104b06811a4018491feeee22177a30fe890a71
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-04-09 14:08:47 -0700

    Update bot_communication.py

--- bot_communication.py
+++ bot_communication.py
@@ -173,7 +173,7 @@ class Diplomacy_Press_Player:
   def init_communication(self, power_list):
     self.bot_type={}
     for power in power_list:
-      self.stance[power] = {power: 'N' for power in power_list}
+      self.stance[power] = {power: 0 for power in power_list}
       self.bot_type[power]= self.bot_temp.pop()
       print('power: '+power+' as ', self.bot_type[power])
       
@@ -185,10 +185,10 @@ class Diplomacy_Press_Player:
      previous_order = game.ordered_units[other_power]
      for order in previous_order:
       if self.get_order_type(game, order, other_power,power_name) =='attack':
-       self.set_stance[power_name][other_power] = 'B'
+       self.stance[power_name][other_power] = -10
       else:
-       if self.get_order_type(game, order, other_power,power_name) =='support' and not self.stance[power_name][other_power] != 'B':
-        self.set_stance[power_name][other_power] = 'A'
+       if self.get_order_type(game, order, other_power,power_name) =='support' and not self.stance[power_name][other_power] <-1:
+        self.stance[power_name][other_power] = 10
          
   @gen.coroutine 
   def get_orders(self, game , power_name):
