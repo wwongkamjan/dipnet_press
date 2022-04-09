3bdd6f066b4222a983d2a35da497a5f7c17fc99f
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-04-09 16:49:57 -0700

    Update DiplomacyEnv.py

--- RL_comm_agent/DiplomacyEnv.py
+++ RL_comm_agent/DiplomacyEnv.py
@@ -60,7 +60,7 @@ class DiplomacyEnv(gym.Env):
     self.ep_info = []
     self.ep_n_states = []
     self.ep_dones = []
-    self.dip_player =  Diplomacy_Press_Player(Player=DipNetSLPlayer())
+    self.dip_player =  Diplomacy_Press_Player(bots=['RL','RL','RL','RL','RL','RL','RL'], Player=DipNetSLPlayer())
     self.dip_game =  Diplomacy_Press(Game=Game(), Player=self.dip_player)
     self.dip_player.init_communication(self.dip_game.powers)
     self.power_mapping = {power: id for power,id in zip(self.dip_game.powers,self.agent_id)}
