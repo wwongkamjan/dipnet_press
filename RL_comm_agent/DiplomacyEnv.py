3c14d7e4eccdcad313c9ab66c8b2a94fa150f859
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-04-09 16:37:30 -0700

    Update DiplomacyEnv.py

--- RL_comm_agent/DiplomacyEnv.py
+++ RL_comm_agent/DiplomacyEnv.py
@@ -55,7 +55,6 @@ class DiplomacyEnv(gym.Env):
     # return to initial state - Diplomacy game and DipNet reset
     # get stance vector, orders from framework
     stop_io_loop()
-    start_io_loop()
     self.ep_states = []
     self.ep_actions = []
     self.ep_rewards = []
