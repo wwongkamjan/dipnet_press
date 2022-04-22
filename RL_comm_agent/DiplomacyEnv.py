632ec40cd51cb3e908c26e5df5a1014ca8fb5bb6
wwongkamjan <w.wongkamjan@gmail.com>
2022-04-22 13:55:23 -0700

    train with discount ally reward

--- RL_comm_agent/DiplomacyEnv.py
+++ RL_comm_agent/DiplomacyEnv.py
@@ -4,7 +4,7 @@ from tornado import gen
 from diplomacy import Game
 from diplomacy.engine.message import Message
 from diplomacy.utils.export import to_saved_game_format
-from diplomacy_research.players.benchmark_player import DipNetSLPlayer
+from diplomacy_research.players.benchmark_player import DipNetSLPlayer, DipNetRLPlayer
 from diplomacy_research.utils.cluster import start_io_loop, stop_io_loop
 from diplomacy_research.models.state_space import get_order_tokens
 from bot_communication import Diplomacy_Press, Diplomacy_Press_Player
